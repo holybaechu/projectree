309699acec378c546b95ccfca2d8ce3e29213673
holybaechu <holybaechu@proton.me>
2026-03-11 20:11:41 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+    let count = 0;
+
+    function onClick() {
+        count++;
+    }
+</script>
+
+
+<!-- 반응형 웹 -->
+<Button onclick={onClick}>{count}</Button>
